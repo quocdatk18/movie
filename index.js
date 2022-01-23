d6e0343dc1df37c65511f430f2ab7e8ef25d3403
quocdatk18 <80533401+quocdatk18@users.noreply.github.com>
2022-01-23 21:00:06 +0700

    update

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express()
 dotenv.config();
 connectDB();
 
-app.use(cors())
+var corsOptions = {
+    origin: 'https://client-movie.netlify.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
