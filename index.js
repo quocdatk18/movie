0064c8e004dd9701cc6d03fe4a3dce9b46a4c994
quocdatk18 <80533401+quocdatk18@users.noreply.github.com>
2022-01-23 21:23:30 +0700

    update fix bug

--- index.js
+++ index.js
@@ -11,21 +11,7 @@ const app = express()
 dotenv.config();
 connectDB();
 
-const allowedOrigins = ['http://localhost:3000',
-  'https://client-movie.netlify.app/'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
