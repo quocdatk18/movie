37648789feac67ef6ad5b9eb48a620497aa2fb42
quocdatk18 <quocdatlop109@gmail.com>
2022-01-23 09:49:50 +0700

    update fix bug

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config()
 async function connectDB() {
     const url = 'mongodb://localhost:27017/videos'
     try {
-        await mongoose.connect(process.nv.DATABASE_URL || url, {
+        await mongoose.connect(process.env.DATABASE_URL || url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
 
